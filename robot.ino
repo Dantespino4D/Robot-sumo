93af1b5f1886a032db2982147a86e7b69a223b3b
DanteEspino4D <dantealerojas45@gmail.com>
2025-09-20 13:55:41 -0600

    correcciones(no funciona)

--- robot.ino
+++ robot.ino
@@ -3,6 +3,10 @@
 #include <Musica.h>
 #include <NewPing.h>
 #include <Wire.h>
+// variables que establecen el tiemṕo
+// unsigned long temp1 = 0;
+// unsigned long temp2 = 0;
+
 // bandera(no recuerdo que hace)
 int banc = 0;
 // alerta del limite
@@ -25,7 +29,7 @@ int sda_1 = 21;
 int scl_1 = 22;
 int led_2 = 5;
 int sda_2 = 18;
-int scl_2 = 5;
+int scl_2 = 23;
 int ena_1 = 33; // quizas no se use
 int ena_2 = 32;
 int swi = 4;
@@ -45,20 +49,19 @@ NewPing ojos_2(trig_2, echo_2, maxd);
 uint16_t lcr = 800, lcg = 700, lcb = 500;
 
 // funcion que avanza en la direccion a(por definir en el robot fisico)
-void dir_a(int x) {
+void dir_a() {
   alto();
   digitalWrite(mot[0][0], HIGH);
   digitalWrite(mot[1][0], HIGH);
 }
 // funcion que avanza en la direccion b(por definir en el robot fisico)
 void dir_b() {
-  alto();
   digitalWrite(mot[1][1], HIGH);
   digitalWrite(mot[0][1], HIGH);
 }
 // gira al robot
-void giro(int x) {
-  alto() digitalWrite(mot[1][0], HIGH);
+void giro() {
+  digitalWrite(mot[1][0], HIGH);
   digitalWrite(mot[0][1], HIGH);
 }
 // para el robot
@@ -73,14 +76,19 @@ void alto() {
 
 void robot(void *pvParameters) {
   while (1) {
+    unsigned long temp1 = 0;
+    unsigned long temp2 = 0;
+
     // prende al precionar el boton
     if (digitalRead(ini) == HIGH) {
       start = true;
+      auxilio();
     }
     // inicia
     while (start) {
-      unsigned long temp1 = 0;
-      unsigned long temp2 = 0;
+
+      int dist_1 = ojos_1.ping_cm();
+      int dist_2 = ojos_2.ping_cm();
       if (!memo1) {
         temp1 = millis();
       }
@@ -106,20 +114,20 @@ void robot(void *pvParameters) {
       else if (false) {
         modo = 1;
       }
-      // si detecta al robot por ojos 1
-      else if (memo1 && ojos_1.ping_cm == 0) {
+      // si deja de detectar al robot por ojos 1
+      else if (memo1 && dist_1 == 0) {
         modo = 2;
       }
-      // si detecta al robot por ojos 2
-      else if (memo2 && ojos_2.ping_cm == 0) {
+      // si deja de detectar al robot por ojos 2
+      else if (memo2 && dist_2 == 0) {
         modo = 3;
       }
-      // si deja de detectar el robot por ojos 1
-      else if (ojos_1.ping_cm != 0) {
+      // si detecta el robot por ojos 1
+      else if (dist_1 != 0) {
         modo = 4;
       }
-      // si deja de detectar el robot por ojos 2
-      else if (ojos_2.ping_cm != 0) {
+      // si detecta el robot por ojos 2
+      else if (dist_2 != 0) {
         modo = 5;
       }
       // si no detecta nada
@@ -140,20 +148,20 @@ void robot(void *pvParameters) {
       // avanza por un tiempo definido de 4 segundo en direccion a
       case 2:
         dir_a();
-        memo1 = true;
         break;
       // avanza por un tiempo definido de 4 segundos en direccion b
       case 3:
         dir_b();
-        memo2 = true;
         break;
       // avanza en direccion a
       case 4:
         dir_a();
+        memo1 = true;
         break;
       // avanza en direccion b
       case 5:
         dir_b();
+        memo2 = true;
         break;
       // da vueltas hasta encontrar el robot
       case 6:
@@ -194,7 +202,8 @@ void calCol() {}
 
 // setup
 void setup() {
-  se crea la alerta alerta = xSemaphoreCreateBinary();
+  // se crea la alerta
+  alerta = xSemaphoreCreateBinary();
   // se inicializan los pines
   pinMode(echo_1, INPUT);
   pinMode(echo_2, INPUT);
@@ -217,7 +226,9 @@ void setup() {
   pinMode(mot[0][1], OUTPUT);
   pinMode(mot[1][0], OUTPUT);
   pinMode(mot[1][1], OUTPUT);
+
   digitalWrite(led_1, HIGH);
+  digitalWrite(led_2, HIGH);
 
   // verifica el funcionamiento de sc_1
   if (sc_1.begin()) {
